592e1951f06f7804219a67d3742e3923fa867c54
Lucas G <lucasg.devmail93@gmail.com>
2022-06-22 17:19:03 -0300

    exercicio-3-calculo-media-do-array / adicionado ";" exercicio-2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-3-calcular-e-imprime-media-aritmetica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-3-calcular-e-imprime-media-aritmetica.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 
-for(data of numbers){
+for (data of numbers) {
     result += data;
 }
 
-console.log(result);
+console.log(result / numbers.length);
