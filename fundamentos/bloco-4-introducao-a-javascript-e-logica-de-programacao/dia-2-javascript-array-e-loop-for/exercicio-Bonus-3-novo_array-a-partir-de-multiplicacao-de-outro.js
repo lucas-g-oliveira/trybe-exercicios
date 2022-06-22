d7b84082ef8321cb0c6541f823b94387df65c9d4
Lucas G <lucasg.devmail93@gmail.com>
2022-06-22 19:52:38 -0300

    exercicio-Bonus-3-criar-arrar-multiplicando-por-2-ressultador-do-outro-array

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-Bonus-3-novo_array-a-partir-de-multiplicacao-de-outro.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let new_array = [];
+
+for (let index = 0; index < numbers.length; index += 1){
+
+
+    if(index == numbers.length-1){
+        console.log('caiu : '+ index);
+        new_array.push(numbers[index]*2);
+        break;
+    }{
+        new_array.push(numbers[index]*numbers[index+1]);
+        console.log('caiu z : '+ index);
+    }
+
+}
+
+console.log(new_array);
