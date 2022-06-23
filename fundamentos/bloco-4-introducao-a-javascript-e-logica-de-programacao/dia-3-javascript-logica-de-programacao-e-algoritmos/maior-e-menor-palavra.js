52a55b73646930d8347ce310fd88dda62d3ba56c
Lucas G <lucasg.devmail93@gmail.com>
2022-06-23 17:16:04 -0300

    maior-e-menor--palavra

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/maior-e-menor-palavra.js
@@ -0,0 +1,22 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = array[0];
+let menor = array[0];
+
+//achar a maior e a menor palavra
+
+for (index = 0; index < array.length; index += 1) {
+
+    //maior
+    if (array[index].length > maior.length) {
+        maior = array[index];
+    }
+
+    //menor
+    if (array[index].length < menor.length && array[index].length > 0 ) {
+        menor = array[index];
+    }
+}
+
+console.log('Maior: ' + maior);
+console.log('Menor: ' + menor);
