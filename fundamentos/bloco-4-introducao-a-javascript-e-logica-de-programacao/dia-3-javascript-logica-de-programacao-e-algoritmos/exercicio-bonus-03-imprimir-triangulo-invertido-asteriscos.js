3cdea1829261a33bb907c47fe95ba92d68f11c61
Lucas G <lucasg.devmail93@gmail.com>
2022-06-25 18:21:28 -0300

    exercicio-bonus-03-imprimir-triangulo-invertido-asteriscos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-03-imprimir-triangulo-invertido-asteriscos.js
@@ -0,0 +1,22 @@
+const numero = 5;
+
+
+for (let index = numero; index >= 1; index -= 1) {
+    
+    let temp = '';
+
+    for (let index2 = 1; index2 <= numero; index2 += 1) {
+
+        if(index2 >= index){
+            temp += '*';
+        }else{
+            temp += ' ';
+        }
+
+    }
+
+    console.log(temp);
+    temp = '';
+
+}
+
