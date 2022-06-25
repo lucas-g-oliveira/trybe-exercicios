dc5df56685259bc0ef67f1ba50236d4a5b10412b
Lucas G <lucasg.devmail93@gmail.com>
2022-06-25 16:55:39 -0300

    exercicio-bonus-02-imprimir-triangulo-asteriscos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio-bonus-02-imprimir-triangulo-asteriscos.js
@@ -0,0 +1,10 @@
+const numero = 5;
+let temp = '';
+
+for (let index = numero; index >= 1; index -= 1) {
+    temp += '*';
+    console.log(temp);
+}
+
+
+//  for (let index = numero; index >= 1; index -= 1) { console.log(temp); }
