d9093874b8917e548cb3fd08190eb8884238a347
Lucas G <lucasg.devmail93@gmail.com>
2022-07-01 19:56:25 -0300

    criação dos arquivos html js e css

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,27 @@
+/*
+Aprofunde seus conhecimentos
+Imagine que você é a pessoa responsável por desenvolver uma página que servirá como um leitor de conteúdo escrito.
+Esse conteúdo escrito pode ser uma página de livro, uma reportagem de revista ou uma nota de jornal online. Para que você não tenha que pensar no conteúdo da página, utilize este link para gerar o texto para sua página.
+Até aqui, nenhuma novidade, mas essa demanda exige que você aplique preferências das pessoas leitoras na página para melhorar a experiência de leitura dessas pessoas.
+As pessoas devem ter o poder de alterar:
+
+1 - Cor de fundo da tela;
+2 - Cor do texto;
+3 - Tamanho da fonte;
+4 - Espaçamento entre as linhas do texto;
+5 - Tipo da fonte (Font family).
+
+Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
+Bônus
+As propriedades descritas acima são obrigatórias, mas você é livre para adicionar qualquer outra propriedade que julgar válida e que tenha como objetivo a melhora da experiência da pessoa que lê em sua página.
+*/
+
+/*
+como farei?
+
+1 - criarei o layout simples da pagina aplicando grid-layout e o outro que esqueci no css, usarei o outro projeto pra me basear
+2 - usarei botões criados no fotoshop para ficar mais intuitivo
+3 - usarei uma unica função para manipular os dados do json e da lista temporario ("caso eu coloque lista")
+4 - colocarei um modo noturno, trocando as classes padrão, porém mantendo a fonte
+5 - o icone da imagem deve mudar (só atribuir outro endereço do link da imagem)
+*/
