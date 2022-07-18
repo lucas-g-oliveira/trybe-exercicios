a06e80aebe88324de72bd603fda72c16eff104c1
Lucas G <lucasg.devmail93@gmail.com>
2022-07-18 19:13:43 -0300

    primeiro commit - criação dos arquivos

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3/script.js
@@ -0,0 +1,6 @@
+/* 
+Link para o enunciado
+
+https://app.betrybe.com/course/fundamentals/introducao-a-javascript-es6-e-testes-unitarios/javascript-es6-fluxo-de-excecao-e-objetos/d9bffb5f-e6b0-4807-9b64-30741a3d3d70/exercicios/bca78ce5-dc65-4e13-9326-29e6edea579f/parte-iii/285a661c-705b-48a3-9ccf-ed76a855d2aa?use_case=side_bar
+
+*/
